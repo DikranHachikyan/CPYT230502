08a4b398a157716bf212d7ed6d239ed6aafd72ef
Dikran H <dhachikian@expert-bg.org>
2023-05-02 21:13:38 +0300

    Python L01

---
+++ ex02_zan01.py
@@ -0,0 +1,24 @@
+# %%
+print('Hello Python')
+
+# %%
+print(5 + 6)
+# %%
+a = 5.0
+b = 6
+
+print(a + b)
+# !!! крайно лоша идея !!!
+# a = 'Hello Python'
+# %%
+
+print( type(a) )
+
+# %%
+print(id(a))
+# %%
+
+a = 1000.0
+
+print(id(a))
+# %%
---
+++ ex04_zan01.py
@@ -0,0 +1,32 @@
+# %%
+s1 = 'Lorem ipsum dolor sit amet'
+
+s2 = "Lorem ipsum\ndolor sit amet"
+
+print(s1)
+print(s2)
+# %%
+s3 = '''Lorem ipsum dolor sit amet,
+consectetur adipisicing elit, sed do
+eiusmod'''
+
+print(s3)
+# %%
+#######################################
+# Function foo prints Hello Python
+# created: 02.05.2023
+# author:Me
+#######################################
+
+def foo():
+    '''Function foo prints Hello Python
+    created: 02.05.2023
+    author:Me'''
+    print("Hello Python")
+    
+
+# %%
+foo()
+# %%
+print(foo.__doc__)
+# %%
